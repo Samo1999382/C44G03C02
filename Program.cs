be400b4eac42f3a2ba50db417d20c1c0a5242975
armo38 <hamadaarafa1999382@gmail.com>
2025-07-09 19:33:55 +0300

    C44G03C05

--- Program.cs
+++ Program.cs
@@ -1,187 +1,268 @@
 ﻿using System;
-// 6- Write a program that allows the user to insert an integer then print all numbers between 1 to that number.
 
-Console.WriteLine("------ All numbers between 1 and n ------");
-Console.Write("Enter an integer: ");
-int n = Convert.ToInt32(Console.ReadLine());
-for (int i = 1; i <= n; i++)
+// 19- . Write a program that prints an identity matrix using for loop, in other words takes a value n from the user and shows the identity table of size n * n.
+
+Console.WriteLine("------ identity ------");
+Console.Write("Enter the value of n: ");
+int value = Convert.ToInt32(Console.ReadLine());
+int[,] arr1 = new int[value, value];
+for (int i = 0; i < value; i++)
 {
-    Console.Write(i + (i < n ? ", " : ""));
-}
-Console.WriteLine();
-
-// 7- Write a program that allows the user to insert an integer then print a multiplication table up to 12.
-
-Console.WriteLine("------ multiplication table ------");
-Console.Write("Enter an integer: ");
-int num = Convert.ToInt32(Console.ReadLine());
-for (int i = 1; i <= 12; i++)
-{
-    Console.Write(num * i + " ");
-}
-Console.WriteLine();
-
-// 8- Write a program that allows to user to insert number then print all even numbers between 1 to this number
-
-Console.WriteLine("------ even numbers between 1 and n ------");
-Console.Write("Enter an integer: ");
-int number = Convert.ToInt32(Console.ReadLine());
-for (int i = 2; i <= number; i += 2)
-{
-    Console.Write(i + " ");
-}
-Console.WriteLine();
-
-// 9- Write a program that takes two integers then prints the power.
-
-Console.WriteLine("------ exponential ------");
-Console.Write("Enter base: ");
-int baseNum = Convert.ToInt32(Console.ReadLine());
-Console.Write("Enter exponent: ");
-int exponent = Convert.ToInt32(Console.ReadLine());
-int result = 1;
-for (int i = 0; i < exponent; i++)
-{
-    result *= baseNum;
-}
-Console.WriteLine(result);
-
-// 10- Write a program to enter marks of five subjects and calculate total, average and percentage.
-
-Console.WriteLine("------ marks ------");
-Console.Write("Enter marks of five subjects(e.g. 92 93 94 95 96): ");
-string[] marks = Console.ReadLine().Split();
-int total = 0;
-foreach (string mark in marks)
-{
-    total += int.Parse(mark);
-}
-Console.WriteLine($"Total marks = {total}");
-Console.WriteLine($"Average Marks = {total / 5}");
-Console.WriteLine($"Percentage = {total / 5}%");
-// Note: I used an array and a string method here to save time even tho we didn't get to it in our course
-
-// 11- Write a program to input the month number and print the number of days in that month.
-
-Console.WriteLine("------ month days ------");
-Console.Write("Enter month number (1-12): ");
-int month = Convert.ToInt32(Console.ReadLine());
-int days = month switch
-{
-    4 or 6 or 9 or 11 => 30,
-    2 => 28,
-    _ => 31
-};
-Console.WriteLine($"Days in Month: {days}");
-// Note: I assumed that it is not a leap year to feb is 28 days
-
-// 12- Write a program to create a Simple Calculator.
-
-Console.WriteLine("------ simple calculator ------");
-Console.Write("Enter two numbers and operator (e.g., 5 + 3): ");
-string[] parts = Console.ReadLine().Split();
-double a = Convert.ToDouble(parts[0]);
-char op = Convert.ToChar(parts[1]);
-double b = Convert.ToDouble(parts[2]);
-double result1 = op switch
-{
-    '+' => a + b,
-    '-' => a - b,
-    '*' => a * b,
-    '/' => a / b,
-    _ => 0
-};
-Console.WriteLine($"Result: {result1}");
-
-// 13- Write a program to allow the user to enter a string and print the REVERSE of it.
-
-Console.WriteLine("------ reverse a tring ------");
-Console.Write("Enter a string: ");
-string input = Console.ReadLine();
-char[] chars = input.ToCharArray();
-Array.Reverse(chars);
-Console.WriteLine(new string(chars));
-// Note: I could also have looped over it but there is an array method to do that
-
-// 14- Write a program to allow the user to enter int and print the REVERSED of it.
-
-Console.WriteLine("------ reverse an int ------");
-Console.Write("Enter an integer: ");
-int num2 = Convert.ToInt32(Console.ReadLine());
-int reversed = 0;
-while (num2 != 0)
-{
-    reversed = reversed * 10 + num2 % 10;
-    num2 /= 10;
-}
-Console.WriteLine(reversed);
-
-// 15- Write a program in C# Sharp to find prime numbers within a range of numbers.
-
-Console.WriteLine("------ prime numbers ------");
-static bool IsPrime(int n)
-{
-    if (n <= 1) return false;
-    for (int i = 2; i <= Math.Sqrt(n); i++)
+    for (int j = 0; j < value; j++)
     {
-        if (n % i == 0) return false;
+        if (i == j)
+        {
+            arr1[i, j] = 1;
+            Console.Write("1");
+        }
+        else
+        {
+            arr1[i, j] = 0;
+            Console.Write("0");
+        }
+        Console.Write(" ");
     }
-    return true;
+    Console.WriteLine("");
+}
+
+// 20- Write a program in C# Sharp to find the sum of all elements of the array.
+
+Console.WriteLine("------ arr sum ------");
+int[] arr2_1d = {1,2,3,4,5,6};
+double[,] arr2_2d = {{1,2,3,4,5,6}, {7,8,9,10,11,12}};
+int[,,] arr2_3d = {{{1,2,3,4,5}, {6,7,8,9,10}}, {{11,12,13,14,15}, {16,17,18,19,20}}};
+
+var sum = 0;
+
+double ArrSum(Array arr)
+{
+    foreach (var item in arr2_1d)
+    {
+        sum += item;
+    }
+    return sum;
+}
+
+Console.WriteLine(ArrSum(arr2_1d));
+Console.WriteLine(ArrSum(arr2_2d));
+Console.WriteLine(ArrSum(arr2_3d));
+// Note: it works for any dimensionality
+
+// 21- Write a program in C# Sharp to merge two arrays of the same size sorted in ascending order.
+
+Console.WriteLine("------ MergeSort ------");
+int[] arr3_1 = {1,3,6,2};
+int[] arr3_2 = {9,7,4,5,8};
+
+static int[] MergeAndSortTwoArrays(int[] arr1, int[] arr2)
+{
+    int[] combined = new int[arr1.Length + arr2.Length];
+    Array.Copy(arr1, 0, combined, 0, arr1.Length);
+    Array.Copy(arr2, 0, combined, arr1.Length, arr2.Length);
+
+    MergeSort(combined);
+
+    return combined;
+}
+
+static void MergeSort(int[] array)
+{
+    if (array.Length <= 1)
+        return;
+
+    int[] temp = new int[array.Length];
+    MergeSortRecursive(array, temp, 0, array.Length - 1);
+}
+
+static void MergeSortRecursive(int[] array, int[] temp, int left, int right)
+{
+    if (left < right)
+    {
+        int mid = left + (right - left) / 2;
+        MergeSortRecursive(array, temp, left, mid);
+        MergeSortRecursive(array, temp, mid + 1, right);
+        Merge(array, temp, left, mid, right);
+    }
+}
+
+static void Merge(int[] array, int[] temp, int left, int mid, int right)
+{
+    for (int i = left; i <= right; i++)
+    {
+        temp[i] = array[i];
+    }
+
+    int leftStart = left;
+    int rightStart = mid + 1;
+    int current = left;
+
+    while (leftStart <= mid && rightStart <= right)
+    {
+        if (temp[leftStart] <= temp[rightStart])
+        {
+            array[current] = temp[leftStart];
+            leftStart++;
+        }
+        else
+        {
+            array[current] = temp[rightStart];
+            rightStart++;
+        }
+        current++;
+    }
+
+    while (leftStart <= mid)
+    {
+        array[current] = temp[leftStart];
+        current++;
+        leftStart++;
+    }
+}
+
+int[] arr3 = MergeAndSortTwoArrays(arr3_1, arr3_2);
+foreach (var item in arr3)
+{
+    Console.Write(item + " ");
 }
 Console.WriteLine();
+// Note: the mergesort algorithm was used here
 
-Console.Write("Enter start and end of range (e.g., 1 50): ");
-string[] range = Console.ReadLine().Split();
-int start = Convert.ToInt32(range[0]);
-int end = Convert.ToInt32(range[1]);
-Console.Write("Prime numbers: ");
-for (int i = start; i <= end; i++)
+// 22- Write a program in C# Sharp to count the frequency of each element of an array.\
+
+Console.WriteLine("------ item freq ------");
+static void CountFrequency(int[] arr)
 {
-    if (IsPrime(i)) Console.Write(i + " ");
+    Dictionary<int, int> frequencyMap = new Dictionary<int, int>();
+
+    foreach (int num in arr)
+    {
+        if (frequencyMap.ContainsKey(num))
+            frequencyMap[num]++;
+        else
+            frequencyMap[num] = 1;
+    }
+
+    foreach (var pair in frequencyMap)
+    {
+        Console.WriteLine($"{pair.Key} occurs {pair.Value} time");
+    }
 }
 
-// 16- Create a program that asks the user to input three points (x1, y1), (x2, y2), and (x3, y3), and determines whether these points lie on a single straight line.
 
-Console.WriteLine("------ IsCollinear ------");
-Console.WriteLine("Enter coordinates for three points:");
-Console.Write("Enter x1: ");
-double x1 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Enter y1: ");
-double y1 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Enter x2: ");
-double x2 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Enter y2: ");
-double y2 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Enter x3: ");
-double x3 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Enter y3: ");
-double y3 = Convert.ToDouble(Console.ReadLine());
-double area = 0.5 * (x1*(y2 - y3) + x2*(y3 - y1) + x3*(y1 - y2)); // from the internet
-if (Math.Abs(area) < 0.0001)
+int[] arr4 = { 1, 2, 3, 2, 4, 1, 5, 2, 3 };
+Console.WriteLine("Frequency of each element:");
+CountFrequency(arr4);
+// Note: dictionary was used here to store as key and value pairs to make it easier to deal with, and genarics ware used to determine its datatypes
+
+// 23- Write a program in C# Sharp to find maximum and minimum element in an array
+
+Console.WriteLine("------ min & max ------");
+int[] arr5 = { 2, 32, 3, 23, 76 };
+int min = arr5[0];
+int max = arr5[0];
+foreach (int num in arr5)
 {
-    Console.WriteLine("These points are COLLINEAR (lie on the same straight line)");
+    if (num < min)
+        min = num;
+    if (num > max)
+        max = num;
 }
-else
+Console.WriteLine("min: "+min+" max: "+max);
+
+// 24- Write a program in C# Sharp to find the second largest element in an array.
+
+Console.WriteLine("------ second largest ------");
+int[] arr6 = {34, 54, 23, 4, 6};
+max = arr6[0];
+int secmax = arr6[0];
+foreach (int num in arr6)
 {
-    Console.WriteLine("These points are NOT COLLINEAR");
+    if (num > max)
+        max = num;
+    else if (num > secmax)
+        secmax = num;
+}
+Console.WriteLine("Second largest number: "+secmax);
+
+// 25-. Consider an Array of Integer values with size N, having values as in this Example
+
+Console.WriteLine("------ space between ------");
+Console.Write("Enter array elements (space-separated integers):");
+int[] arr7 = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+        
+Dictionary<int, List<int>> indices = new Dictionary<int, List<int>>();
+        
+for (int i = 0; i < arr7.Length; i++)
+{
+    if (!indices.ContainsKey(arr7[i]))
+        indices[arr7[i]] = new List<int>();
+    indices[arr7[i]].Add(i);
+}
+        
+int maxDistance = 0;
+foreach (var item in indices)
+{
+    if (item.Value.Count >= 2)
+    {
+        int distance = item.Value[item.Value.Count - 1] - item.Value[0] - 1;
+        if (distance > maxDistance)
+            maxDistance = distance;
+    }
+}
+        
+Console.WriteLine($"Longest distance between two equal cells: {maxDistance}");
+// Note: sorry i once again used dicts and generics once more
+
+// 26- Given a list of space separated words, reverse the order of the words.
+
+Console.WriteLine("------ reverse ------");
+Console.Write("Enter a sentence:");
+string input = Console.ReadLine();
+        
+string[] words = input.Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+Array.Reverse(words);
+        
+Console.WriteLine("Reversed order: " + string.Join(" ", words));
+
+// 27- Write a program to create two multidimensional arrays of same size. Accept value from user and store them in first array.
+// Now copy all the elements of first array on second array and print second array.
+
+Console.WriteLine("----- coping arrays ------");
+Console.Write("Enter number of rows: ");
+int rows = int.Parse(Console.ReadLine());
+Console.Write("Enter number of columns: ");
+int cols = int.Parse(Console.ReadLine());
+        
+int[,] arr8_1 = new int[rows, cols];
+int[,] arr8_2 = new int[rows, cols];
+        
+Console.WriteLine("Enter matrix elements (row-wise):");
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < cols; j++)
+    {
+        arr8_1[i, j] = int.Parse(Console.ReadLine());
+        arr8_2[i, j] = arr8_1[i, j];
+    }
+}
+        
+Console.WriteLine("\nCopied array:");
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < cols; j++)
+    {
+        Console.Write(arr8_2[i, j]+ " ");
+    }
+    Console.WriteLine();
 }
 
-// 18- Within a company, the efficiency of workers is evaluated based on the duration required to complete a specific task. A worker's efficiency level is determined as follows: 
-// - If the worker completes the job within 2 to 3 hours, they are considered highly efficient. 
-// - If the worker takes 3 to 4 hours, they are instructed to increase their speed. 
-// - If the worker takes 4 to 5 hours, they are provided with training to enhance their speed. 
-// - If the worker takes more than 5 hours, they are required to leave the company. 
-//     To calculate the efficiency of a worker, the time taken for the task is obtained via user input from the keyboard.
+// 28- Write a Program to Print One Dimensional Array in Reverse Order
 
-Console.WriteLine("------ worker's effixiancy ------");
-Console.Write("Enter time taken (hours): ");
-double hours = Convert.ToDouble(Console.ReadLine());
-string message = hours switch
+Console.WriteLine("------ print reversed array ------");
+int[] arr9 = {1,2,3,4,5,6};
+        
+Console.Write("Array in reverse order:");
+for (int i = arr9.Length - 1; i >= 0; i--)
 {
-    >= 2 and <= 3 => "Highly efficient",
-    > 3 and <= 4 => "Increase speed",
-    > 4 and <= 5 => "Needs training",
-    > 5 => "Terminate employment",
-    _ => "Invalid time"
-};
-Console.WriteLine(message);
+    Console.Write(arr9[i] + " ");
+}
